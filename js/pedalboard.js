a992806469244cda2ef8527e499e6ba9c0a81679
GilgaFrank <44657392+GilgaFrank@users.noreply.github.com>
2022-08-04 13:43:41 +0100

    Modal CSS/JS

--- js/pedalboard.js
+++ js/pedalboard.js
@@ -363,3 +363,31 @@ window.addEventListener('load', (event) => {
     initUI();
 
 });
+
+
+
+// Get the modal
+var divEventideModal = document.getElementById("divEventideModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnShowEventideModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  divEventideModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  divEventideModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == divEventideModal) {
+    divEventideModal.style.display = "none";
+  }
+} 
